f09936562c259a3f4a7b058e4108325cd4e4d5c1
Picalines <picalines0@gmail.com>
2023-02-02 21:55:46 +0300

    fix TransformModelConverter read method

--- SceneRecorder.Shared/Models/JsonConverters/TransformModelConverter.cs
+++ SceneRecorder.Shared/Models/JsonConverters/TransformModelConverter.cs
@@ -10,24 +10,18 @@ internal sealed class TransformModelConverter : JsonConverter<TransformModel>
 
     public override bool CanWrite => true;
 
-    private static readonly float[] _PositionBuffer = new float[3];
-    private static readonly float[] _RotationBuffer = new float[4];
-    private static readonly float[] _ScaleBuffer = new float[3];
-
     public override TransformModel ReadJson(JsonReader reader, Type objectType, TransformModel existingValue, bool hasExistingValue, JsonSerializer serializer)
     {
-        ReadToken(reader, JsonToken.StartArray);
+        var baseArray = JArray.Load(reader);
 
-        ReadFloatArray(reader, _PositionBuffer);
-        ReadFloatArray(reader, _RotationBuffer);
-        ReadFloatArray(reader, _ScaleBuffer);
-
-        ReadToken(reader, JsonToken.EndArray);
+        var positionArray = ((JArray)baseArray.ElementAt(0)).Values<float>().ToArray();
+        var rotationArray = ((JArray)baseArray.ElementAt(1)).Values<float>().ToArray();
+        var scaleArray = ((JArray)baseArray.ElementAt(2)).Values<float>().ToArray();
 
         var transform = new TransformModel(
-            Position: new Vector3(_PositionBuffer[0], _PositionBuffer[1], _PositionBuffer[2]),
-            Rotation: new Quaternion(_RotationBuffer[0], _RotationBuffer[1], _RotationBuffer[2], _RotationBuffer[3]),
-            Scale: new Vector3(_ScaleBuffer[0], _ScaleBuffer[1], _ScaleBuffer[2]));
+            Position: new Vector3(positionArray[0], positionArray[1], positionArray[2]),
+            Rotation: new Quaternion(rotationArray[0], rotationArray[1], rotationArray[2], rotationArray[3]),
+            Scale: new Vector3(scaleArray[0], scaleArray[1], scaleArray[2]));
 
         return transform;
     }
@@ -62,26 +56,5 @@ internal sealed class TransformModelConverter : JsonConverter<TransformModel>
         };
 
         array.WriteTo(writer);
-    }
-
-    private static void ReadToken(JsonReader reader, JsonToken expectedTokenType)
-    {
-        if (reader.Read() is false || reader.TokenType != expectedTokenType)
-        {
-            throw new JsonSerializationException($"expected {expectedTokenType} at {reader.Path}");
-        }
-    }
-
-    private static void ReadFloatArray(JsonReader reader, float[] destination)
-    {
-        ReadToken(reader, JsonToken.StartArray);
-
-        for (int i = 0; i < destination.Length; i++)
-        {
-            destination[i] = (float)(reader.ReadAsDouble()
-                ?? throw new JsonSerializationException($"expected float at {reader.Path}"));
-        }
-
-        ReadToken(reader, JsonToken.EndArray);
     }
 }
