5bfe74ae6873fd871cbcb720857281b0a5a9d1bb
Picalines <picalines0@gmail.com>
2023-12-23 21:22:26 +0300

    update ICommonCameraAPI

--- SceneRecorder.Shared/Interfaces/ICommonCameraAPI.cs
+++ SceneRecorder.Shared/Interfaces/ICommonCameraAPI.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Picalines.OuterWilds.SceneRecorder.Shared.Interfaces;
 
 public interface ICommonCameraAPI
 {
-    (OWCamera, Camera) CreateCustomCamera(string name, Action<OWCamera> postInitMethod);
+    void RegisterCustomCamera(OWCamera OWCamera);
+
+    (OWCamera, Camera) CreateCustomCamera(string name);
+
+	(OWCamera, Camera) CreateCustomCamera(string name, Action<OWCamera> postInitMethod);
+
     void ExitCamera(OWCamera OWCamera);
-    void EnterCamera(OWCamera OWCamera);
+
+	void EnterCamera(OWCamera OWCamera);
+
+	UnityEvent<PlayerTool> EquipTool();
+    UnityEvent<PlayerTool> UnequipTool();
 }
