2d3e8083c141a6c5e9ff717a1c094e046e38618f
Picalines <picalines0@gmail.com>
2024-01-12 16:16:39 +0300

    remove spaces

--- SceneRecorder.Infrastructure/API/ICommonCameraAPI.cs
+++ SceneRecorder.Infrastructure/API/ICommonCameraAPI.cs
@@ -8,11 +8,9 @@ public interface ICommonCameraAPI
     void RegisterCustomCamera(OWCamera OWCamera);
 
     (OWCamera, Camera) CreateCustomCamera(string name);
-
     (OWCamera, Camera) CreateCustomCamera(string name, Action<OWCamera> postInitMethod);
 
     void ExitCamera(OWCamera OWCamera);
-
     void EnterCamera(OWCamera OWCamera);
 
     UnityEvent<PlayerTool> EquipTool();
