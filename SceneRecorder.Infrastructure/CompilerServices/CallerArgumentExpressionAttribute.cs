7a9166d6c8040e984c05c7c892964f804f558998
Picalines <picalines0@gmail.com>
2024-01-15 00:58:38 +0300

    backport CallerArgumentExceptionAttribute

---
+++ SceneRecorder.Infrastructure/CompilerServices/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,17 @@
+#if !NET6_0_OR_GREATER
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        public string ParameterName { get; }
+
+        public CallerArgumentExpressionAttribute(string parameterName)
+        {
+            ParameterName = parameterName;
+        }
+    }
+}
+
+#endif
