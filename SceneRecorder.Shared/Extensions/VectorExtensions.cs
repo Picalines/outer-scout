9ead8818b00ca4ad1b1426badd54ec1160f2f433
Picalines <picalines0@gmail.com>
2024-01-19 01:11:29 +0300

    vector deconstruct extensions

---
+++ SceneRecorder.Shared/Extensions/VectorExtensions.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace SceneRecorder.Shared.Extensions;
+
+public static class VectorExtensions
+{
+    public static void Deconstruct(this Vector2 vector, out float x, out float y)
+    {
+        x = vector.x;
+        y = vector.y;
+    }
+
+    public static void Deconstruct(this Vector3 vector, out float x, out float y, out float z)
+    {
+        x = vector.x;
+        y = vector.y;
+        z = vector.z;
+    }
+
+    public static void Deconstruct(
+        this Quaternion quaternion,
+        out float x,
+        out float y,
+        out float z,
+        out float w
+    )
+    {
+        x = quaternion.x;
+        y = quaternion.y;
+        z = quaternion.z;
+        w = quaternion.w;
+    }
+}
