0f51e4cd5031fabd172768abb58313609c2c2219
Picalines <picalines0@gmail.com>
2024-01-28 16:37:43 +0300

    ReversableAction safety exceptions

--- SceneRecorder.Recording/Domain/ReversableAction.cs
+++ SceneRecorder.Recording/Domain/ReversableAction.cs
@@ -6,6 +6,8 @@ public sealed class ReversableAction
 
     private Action? _reverse = null;
 
+    private bool _performed = false;
+
     public ReversableAction(Action perform, Action reverse)
     {
         _perform = perform;
@@ -22,11 +24,29 @@ public sealed class ReversableAction
 
     public void Perform()
     {
+        if (_performed)
+        {
+            throw new InvalidOperationException(
+                $"{nameof(ReversableAction)}.{nameof(Perform)} called twice"
+            );
+        }
+
+        _performed = true;
+
         _perform?.Invoke();
     }
 
     public void Reverse()
     {
+        if (_performed is false)
+        {
+            throw new InvalidOperationException(
+                $"{nameof(ReversableAction)}.{nameof(Reverse)} called before {nameof(Perform)}"
+            );
+        }
+
+        _performed = false;
+
         _reverse?.Invoke();
     }
 }
