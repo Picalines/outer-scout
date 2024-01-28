f14310af33221a7122af9e013967017080754fb9
Picalines <picalines0@gmail.com>
2024-01-28 17:18:35 +0300

    waste extensions

--- SceneRecorder.Infrastructure/Extensions/IEnumerableExtensions.cs
+++ SceneRecorder.Infrastructure/Extensions/IEnumerableExtensions.cs
@@ -20,47 +20,4 @@ public static class IEnumerableExtensions
             yield return (index++, value);
         }
     }
-
-    public static TSource? MinByOrDefault<TSource, TKey>(
-        this IEnumerable<TSource> source,
-        Func<TSource, TKey> selector
-    )
-    {
-        return source.MinByOrDefault(selector, Comparer<TKey>.Default);
-    }
-
-    public static TSource? MinByOrDefault<TSource, TKey>(
-        this IEnumerable<TSource> source,
-        Func<TSource, TKey> selector,
-        IComparer<TKey> comparer
-    )
-    {
-        if (source is null)
-            throw new ArgumentNullException("source");
-        if (selector is null)
-            throw new ArgumentNullException("selector");
-
-        using var sourceIterator = source.GetEnumerator();
-
-        if (!sourceIterator.MoveNext())
-        {
-            return default;
-        }
-
-        var min = sourceIterator.Current;
-        var selectedMin = selector(min);
-
-        while (sourceIterator.MoveNext())
-        {
-            var element = sourceIterator.Current;
-            var selectedElement = selector(element);
-            if (comparer.Compare(selectedElement, selectedMin) < 0)
-            {
-                min = element;
-                selectedMin = selectedElement;
-            }
-        }
-
-        return min;
-    }
 }
