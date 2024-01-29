40d55bb5e6c3be75e82f5cde42761c8d44059cda
Picalines <picalines0@gmail.com>
2024-01-29 23:35:55 +0300

    remove context, use services

--- SceneRecorder.WebApi/IRouteMapper.cs
+++ SceneRecorder.WebApi/IRouteMapper.cs
@@ -1,14 +1,8 @@
-﻿using SceneRecorder.Application.Recording;
-using SceneRecorder.WebApi.Http;
+﻿using SceneRecorder.WebApi.Http;
 
 namespace SceneRecorder.WebApi;
 
 internal interface IRouteMapper
 {
-    public interface IContext
-    {
-        public OutputRecorder OutputRecorder { get; }
-    }
-
-    public void MapRoutes(HttpServerBuilder serverBuilder, IContext context);
+    public void MapRoutes(HttpServer.Builder serverBuilder);
 }
