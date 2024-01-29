6cee289fda46af2a261ee3075b3a09be8fb9f201
Picalines <picalines0@gmail.com>
2024-01-29 23:35:37 +0300

    refactor WebApiServer

--- SceneRecorder.WebApi/WebApiServer.cs
+++ SceneRecorder.WebApi/WebApiServer.cs
@@ -1,21 +1,18 @@
-﻿using OWML.Common;
-using SceneRecorder.Application.Recording;
+﻿using System.Reflection;
+using Newtonsoft.Json;
+using OWML.Common;
+using SceneRecorder.Infrastructure.DependencyInjection;
 using SceneRecorder.Infrastructure.Extensions;
 using SceneRecorder.WebApi.Http;
 using SceneRecorder.WebApi.RouteMappers;
-using UnityEngine;
 
 namespace SceneRecorder.WebApi;
 
-[RequireComponent(typeof(OutputRecorder))]
-public sealed class WebApiServer : HttpServer
+public sealed class WebApiServer : IDisposable
 {
-    private sealed record RouteDefinitionContext(OutputRecorder OutputRecorder)
-        : IRouteMapper.IContext;
-
     private static readonly IRouteMapper[] _RouteMappers = new IRouteMapper[]
     {
-        CameraInfoRouteMapper.Instance,
+        CameraRouteMapper.Instance,
         GroundBodyRouteMapper.Instance,
         KeyframesRouteMapper.Instance,
         RecorderRouteMapper.Instance,
@@ -24,39 +21,64 @@ public sealed class WebApiServer : HttpServer
         WarpRouteMapper.Instance,
     };
 
-    public void Configure(IModConfig modConfig, IModConsole modConsole)
-    {
-        ModConsole = modConfig.GetEnableApiInfoLogsSetting()
-            ? modConsole
-            : modConsole.WithOnlyMessagesOfType(MessageType.Warning, MessageType.Error);
+    private readonly HttpServer _httpServer;
 
-        var listenUrl = $"http://localhost:{modConfig.GetApiPortSetting()}/";
-        var httpServerBuilder = new HttpServerBuilder(listenUrl);
+    public WebApiServer()
+    {
+        var modConfig = Singleton<IModConfig>.Instance;
+
+        var httpServerBuilder = new HttpServer.Builder(
+            $"http://localhost:{modConfig.GetApiPortSetting()}/"
+        );
+
+        ConfigureServices(httpServerBuilder);
 
         MapRoutes(httpServerBuilder);
 
-        if (Listening is true)
-        {
-            StopListening();
-        }
-
-        httpServerBuilder.Build(this);
-        StartListening();
+        _httpServer = httpServerBuilder.Build();
     }
 
-    private void MapRoutes(HttpServerBuilder serverBuilder)
+    public void Dispose()
     {
-        var context = new RouteDefinitionContext(GetComponent<OutputRecorder>());
+        _httpServer.Dispose();
+    }
 
+    private void ConfigureServices(HttpServer.Builder serverBuilder)
+    {
+        var modConfig = Singleton<IModConfig>.Instance;
+        var modConsole = Singleton<IModConsole>.Instance;
+
+        serverBuilder.Services.RegisterInstance<IModConsole>(
+            modConfig.GetEnableApiInfoLogsSetting()
+                ? modConsole
+                : modConsole.WithOnlyMessagesOfType(MessageType.Warning, MessageType.Error)
+        );
+
+        serverBuilder.Services.RegisterInstance(
+            new JsonSerializer()
+            {
+                MissingMemberHandling = MissingMemberHandling.Error,
+                Converters =
+                {
+                    Assembly
+                        .GetExecutingAssembly()
+                        .GetTypes()
+                        .Where(type => type.IsAbstract is false)
+                        .Where(type => type.IsSubclassOf(typeof(JsonConverter)))
+                        .Select(type => (JsonConverter)Activator.CreateInstance(type))
+                },
+            }
+        );
+    }
+
+    private void MapRoutes(HttpServer.Builder serverBuilder)
+    {
         foreach (var mapper in _RouteMappers)
         {
-            mapper.MapRoutes(serverBuilder, context);
+            mapper.MapRoutes(serverBuilder);
         }
 
-        serverBuilder.MapGet(
-            "",
-            () => new { Message = $"Welcome to Outer Wilds {nameof(SceneRecorder)} API!" }
-        );
+        serverBuilder.MapGet("", () => $"Welcome to Outer Wilds {nameof(SceneRecorder)} API!");
 
         serverBuilder.MapGet("api-status", () => new { Available = true });
     }
