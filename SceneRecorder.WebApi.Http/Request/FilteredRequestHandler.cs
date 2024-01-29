7a7a077c2a8d0fc6c9341f76beb9e6bcd23d880d
Picalines <picalines0@gmail.com>
2024-01-29 04:53:34 +0300

    handle exceptions in FilteredRequestHandler

---
+++ SceneRecorder.WebApi.Http/Request/FilteredRequestHandler.cs
@@ -0,0 +1,44 @@
+using System.Reflection;
+using Newtonsoft.Json;
+using SceneRecorder.WebApi.Http.Response;
+
+namespace SceneRecorder.WebApi.Http;
+
+internal sealed class FilteredRequestHandler : IRequestHandler
+{
+    private readonly IRequestHandler _requestHandler;
+
+    public FilteredRequestHandler(IRequestHandler requestHandler)
+    {
+        _requestHandler = requestHandler;
+    }
+
+    public IResponse Handle(Request request)
+    {
+        try
+        {
+            return _requestHandler.Handle(request);
+        }
+        catch (JsonSerializationException jsonException)
+        {
+            return ResponseFabric.BadRequest(jsonException.Message);
+        }
+        catch (Exception internalException)
+        {
+            int depth = 0;
+
+            while (
+                internalException
+                    is TargetInvocationException { InnerException: var innerException }
+                && ++depth < 100
+            )
+            {
+                internalException = innerException;
+            }
+
+            return ResponseFabric.InternalServerError(
+                $"{internalException.GetType()}: {internalException.Message}\n{internalException.StackTrace}"
+            );
+        }
+    }
+}
