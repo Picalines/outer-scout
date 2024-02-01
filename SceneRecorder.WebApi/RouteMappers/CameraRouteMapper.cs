b6b2032116c125cd3996567501869a31fb5422d2
Picalines <picalines0@gmail.com>
2024-02-01 23:36:42 +0300

    camera route stubs

---
+++ SceneRecorder.WebApi/RouteMappers/CameraRouteMapper.cs
@@ -0,0 +1,42 @@
+﻿using SceneRecorder.WebApi.DTOs;
+using SceneRecorder.WebApi.Extensions;
+using SceneRecorder.WebApi.Http;
+using SceneRecorder.WebApi.Http.Response;
+
+namespace SceneRecorder.WebApi.RouteMappers;
+
+using static ResponseFabric;
+
+internal sealed class CameraRouteMapper : IRouteMapper
+{
+    public static CameraRouteMapper Instance { get; } = new();
+
+    private CameraRouteMapper() { }
+
+    public void MapRoutes(HttpServer.Builder serverBuilder)
+    {
+        using (serverBuilder.UseInPlayableSceneFilter())
+        {
+            serverBuilder.MapPost("cameras", CreateSceneCamera);
+
+            serverBuilder.MapGet("cameras/:id/perspective-info", GetCameraPerspectiveInfo);
+
+            serverBuilder.MapPut("cameras/:id/perspective-info", PutCameraPerspectiveInfo);
+        }
+    }
+
+    private static IResponse CreateSceneCamera([FromBody] ISceneCameraDTO cameraDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IResponse GetCameraPerspectiveInfo(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IResponse PutCameraPerspectiveInfo(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
