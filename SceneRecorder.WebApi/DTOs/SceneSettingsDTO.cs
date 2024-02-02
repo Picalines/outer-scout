a6b5a198b21511e0e37b81ef8fc4ae321331f9d0
Picalines <picalines0@gmail.com>
2024-02-03 00:36:02 +0300

    remove OutputDirectory

--- SceneRecorder.WebApi/DTOs/SceneSettingsDTO.cs
+++ SceneRecorder.WebApi/DTOs/SceneSettingsDTO.cs
@@ -2,8 +2,6 @@
 
 internal sealed class SceneSettingsDTO
 {
-    public required string OutputDirectory { get; init; }
-
     public required SceneFramesDTO Frames { get; init; }
 
     public required bool HidePlayerModel { get; init; }
