58e2f0405ac3eb494018eb3d2b80a60a75fafb94
Picalines <picalines0@gmail.com>
2024-02-02 23:28:48 +0300

    remove SceneSingleton, use SceneService

--- SceneRecorder.WebApi/Components/SceneService.cs
+++ SceneRecorder.WebApi/Components/SceneService.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace SceneRecorder.WebApi.Components;
 
-internal sealed class SceneService<T>(Func<T> instanceFactory) : IService<T>
+internal sealed class SceneService<T>(Func<T> instanceFactory) : IService<T>, IDisposable
 {
     private ApiResource<T>? _resource = null;
 
@@ -19,5 +19,25 @@ internal sealed class SceneService<T>(Func<T> instanceFactory) : IService<T>
         }
 
         return _resource.Value;
+    }
+
+    public void Dispose()
+    {
+        if (_resource is { IsAccessable: true } and IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }
+}
+
+internal static class SceneServiceExtensions
+{
+    public static IDisposable RegisterSceneInstance<T>(
+        this ServiceContainer services,
+        Func<T> instanceFactory
+    )
+        where T : class
+    {
+        return services.RegisterService(new SceneService<T>(instanceFactory));
     }
 }
--- SceneRecorder.WebApi/Extensions/HttpServerBuilderExtensions.cs
+++ SceneRecorder.WebApi/Extensions/HttpServerBuilderExtensions.cs
@@ -6,9 +6,9 @@ namespace SceneRecorder.WebApi.Extensions;
 
 internal static class HttpServerBuilderExtensions
 {
-    public static IDisposable UseInPlayableSceneFilter(this HttpServer.Builder serverBuilder)
+    public static IDisposable WithPlayableSceneFilter(this HttpServer.Builder serverBuilder)
     {
-        return serverBuilder.UseFilter(request =>
+        return serverBuilder.WithFilter(request =>
         {
             return LocatorExtensions.IsInPlayableScene() is false
                 ? ResponseFabric.ServiceUnavailable(new { Error = "not in playable scene" })
