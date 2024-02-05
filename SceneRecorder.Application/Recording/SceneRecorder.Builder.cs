6e098a8b3cfb734b1467ace1404600b88846449f
Picalines <picalines0@gmail.com>
2024-02-06 00:40:13 +0300

    set Time.captureFramerate while recording

--- SceneRecorder.Application/Recording/SceneRecorder.Builder.cs
+++ SceneRecorder.Application/Recording/SceneRecorder.Builder.cs
@@ -1,5 +1,7 @@
 using SceneRecorder.Application.Animation;
 using SceneRecorder.Domain;
+using SceneRecorder.Infrastructure.Validation;
+using UnityEngine;
 
 namespace SceneRecorder.Application.Recording;
 
@@ -9,11 +11,28 @@ public sealed partial class SceneRecorder
     {
         private IntRange _frameRange = IntRange.FromValues(1, 100);
 
+        private int _captureFrameRate = 30;
+
         private readonly HashSet<IAnimator> _animators = [];
 
         private readonly List<Func<IRecorder>> _recorderFactories = [];
 
         private readonly List<ReversableAction> _scenePatches = [];
+
+        public Builder()
+        {
+            WithCaptureFrameRatePatch();
+        }
+
+        public IntRange FrameRange
+        {
+            get => _frameRange;
+        }
+
+        public int CaptureFrameRate
+        {
+            get => _captureFrameRate;
+        }
 
         public SceneRecorder StartRecording()
         {
@@ -25,14 +44,16 @@ public sealed partial class SceneRecorder
             );
         }
 
-        public IntRange FrameRange
-        {
-            get => _frameRange;
-        }
-
         public Builder WithFrameRange(IntRange frameRange)
         {
             _frameRange = frameRange;
+            return this;
+        }
+
+        public Builder WithCaptureFrameRate(int frameRate)
+        {
+            frameRate.Throw().IfLessThan(1);
+            _captureFrameRate = frameRate;
             return this;
         }
 
@@ -52,6 +73,18 @@ public sealed partial class SceneRecorder
         {
             _scenePatches.Add(reversableAction);
             return this;
+        }
+
+        private Builder WithCaptureFrameRatePatch()
+        {
+            return WithScenePatch(
+                new(() =>
+                {
+                    var oldCaptureFrameRate = Time.captureFramerate;
+                    Time.captureFramerate = _captureFrameRate;
+                    return () => Time.captureFramerate = oldCaptureFrameRate;
+                })
+            );
         }
     }
 }
