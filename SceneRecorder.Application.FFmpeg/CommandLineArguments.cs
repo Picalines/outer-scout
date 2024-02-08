71a99eceb3f8b7ceba425218e61e9d919b96dfd2
Picalines <picalines0@gmail.com>
2024-02-08 18:24:23 +0300

    private field convention

--- SceneRecorder.Application.FFmpeg/CommandLineArguments.cs
+++ SceneRecorder.Application.FFmpeg/CommandLineArguments.cs
@@ -4,23 +4,23 @@ namespace SceneRecorder.Application.FFmpeg;
 
 internal readonly ref struct CommandLineArguments
 {
-    private readonly StringBuilder _StringBuilder;
+    private readonly StringBuilder _stringBuilder;
 
     public CommandLineArguments()
     {
-        _StringBuilder = new StringBuilder();
+        _stringBuilder = new StringBuilder();
     }
 
     public CommandLineArguments Add(string argument)
     {
-        _StringBuilder.Append(argument);
-        _StringBuilder.Append(' ');
+        _stringBuilder.Append(argument);
+        _stringBuilder.Append(' ');
         return this;
     }
 
     public override string ToString()
     {
-        _StringBuilder.Remove(_StringBuilder.Length - 1, 1);
-        return _StringBuilder.ToString();
+        _stringBuilder.Remove(_stringBuilder.Length - 1, 1);
+        return _stringBuilder.ToString();
     }
 }
