2ba57197b17559f943bfd9400f6b8866fba250b0
Picalines <picalines0@gmail.com>
2024-03-13 21:24:19 +0300

    refactor /recorders to use "property" in request body

--- OuterScout.WebApi/Endpoints/RecorderEndpoint.cs
+++ OuterScout.WebApi/Endpoints/RecorderEndpoint.cs
@@ -2,36 +2,30 @@ using Newtonsoft.Json;
 using OuterScout.Application.FFmpeg;
 using OuterScout.Application.Recording;
 using OuterScout.Application.SceneCameras;
+using OuterScout.Infrastructure.DependencyInjection;
+using OuterScout.Infrastructure.Extensions;
 using OuterScout.WebApi.DTOs;
 using OuterScout.WebApi.Extensions;
 using OuterScout.WebApi.Http;
 using OuterScout.WebApi.Http.Response;
 using OuterScout.WebApi.Services;
+using UnityEngine;
 
 namespace OuterScout.WebApi.Endpoints;
 
 using static ResponseFabric;
 
-internal sealed class RecorderEndpoint : IRouteMapper
+internal sealed class RecorderEndpoint : IRouteMapper, IServiceConfiguration
 {
+    private const string RecorderScope = "request.recorder";
+
     public static RecorderEndpoint Instance { get; } = new();
 
     private RecorderEndpoint() { }
 
-    private sealed class CreateTextureRecorderRequest
+    private class BaseRequestBody
     {
-        public required string OutputPath { get; init; }
-
-        public int ConstantRateFactor { get; init; } = 18;
-    }
-
-    private sealed class CreateTransformRecorderRequest
-    {
-        public required string OutputPath { get; init; }
-
-        public required string Format { get; init; }
-
-        public required string Parent { get; init; }
+        public required string Property { get; init; }
     }
 
     public void MapRoutes(HttpServer.Builder serverBuilder)
@@ -40,128 +34,272 @@ internal sealed class RecorderEndpoint : IRouteMapper
         using (serverBuilder.WithSceneCreatedFilter())
         using (serverBuilder.WithNotRecordingFilter())
         {
-            serverBuilder.MapPost(
-                "cameras/:cameraId/textures/:textureType/recorder",
-                PostCameraTextureRecorder
-            );
+            serverBuilder.MapPost("cameras/:id/recorders", PostCameraRecorder);
 
-            serverBuilder.MapPost(
-                "gameObjects/:gameObjectName/transform/recorder",
-                PostTransformRecorder
-            );
+            serverBuilder.MapPost("gameObjects/:name/recorders", PostGameObjectRecorder);
         }
     }
 
-    private static IResponse PostCameraTextureRecorder(
-        [FromUrl] string cameraId,
-        [FromUrl] string textureType,
-        [FromBody] CreateTextureRecorderRequest request,
-        ApiResourceRepository resources
+    public void RegisterServices(ServiceContainer.Builder services)
+    {
+        using (services.InScope(RecorderScope))
+        {
+            services.Register<PostTransformRecorderHandler>().As<IPostRecorderHandler>();
+            services.Register<PostColorTextureRecorderHandler>().As<IPostRecorderHandler>();
+            services.Register<PostDepthTextureRecorderHandler>().As<IPostRecorderHandler>();
+        }
+    }
+
+    private static IResponse PostCameraRecorder(
+        [FromUrl] string id,
+        Request request,
+        JsonSerializer jsonSerializer,
+        ApiResourceRepository resources,
+        IServiceScope services
     )
     {
-        if (FFmpeg.CheckInstallation() is { } exception)
+        if (resources.GlobalContainer.GetResource<ISceneCamera>(id) is not { } camera)
         {
-            return ServiceUnavailable(
-                new { Error = "ffmpeg is not available", Exception = exception }
-            );
+            return NotFound($"camera '{id}' not found");
         }
 
-        if (request.OutputPath.EndsWith(".mp4") is false)
-        {
-            return BadRequest("only .mp4 video output is supported");
-        }
+        var property = ReadPropertyFromBody(request, jsonSerializer);
 
-        if (request.ConstantRateFactor is < 0 or > 63)
-        {
-            return BadRequest("unsupported constant rate factor value");
-        }
+        using var recorderScope = services.StartScope(RecorderScope);
 
-        if (
-            resources.GlobalContainer.GetResource<SceneRecorder.Builder>()
-            is not { } sceneRecorderBuilder
-        )
-        {
-            return ServiceUnavailable();
-        }
+        var handler = recorderScope
+            .ResolveAll<IPostRecorderHandler>()
+            .FirstOrDefault(h => h.Property == property);
 
-        if (resources.GlobalContainer.GetResource<ISceneCamera>(cameraId) is not { } camera)
+        return handler switch
         {
-            return NotFound($"camera '{cameraId}' not found");
-        }
-
-        var renderTexture = textureType switch
-        {
-            "color" => camera.ColorTexture,
-            "depth" => camera.DepthTexture,
-            _ => null,
+            IPostRecorderHandler<ISceneCamera> ch => ch.HandleRequest(camera),
+            _ => BadRequest($"property '{property}' is not recordable"),
         };
-
-        if (renderTexture is null)
-        {
-            return NotFound($"camera '{cameraId}' cannot record {textureType} texture");
-        }
-
-        sceneRecorderBuilder.WithRecorder(
-            new RenderTextureRecorder.Builder(targetFile: request.OutputPath, renderTexture)
-                .WithFrameRate(sceneRecorderBuilder.CaptureFrameRate)
-                .WithConstantRateFactor(request.ConstantRateFactor)
-        );
-
-        return Created();
     }
 
-    private static IResponse PostTransformRecorder(
-        [FromUrl] string gameObjectName,
-        [FromBody] CreateTransformRecorderRequest request,
+    private static IResponse PostGameObjectRecorder(
+        [FromUrl] string name,
+        Request request,
         JsonSerializer jsonSerializer,
         GameObjectRepository gameObjects,
-        ApiResourceRepository resources
+        IServiceScope services
     )
     {
-        if (request.Format is not "json")
+        if (gameObjects.FindOrNull(name) is not { } gameObject)
         {
-            return BadRequest("only .json output is supported");
+            return NotFound($"gameObject '{name}' not found");
         }
 
-        if (
-            resources.GlobalContainer.GetResource<SceneRecorder.Builder>()
-            is not { } sceneRecorderBuilder
-        )
-        {
-            return ServiceUnavailable();
-        }
+        var property = ReadPropertyFromBody(request, jsonSerializer);
 
-        if (gameObjects.FindOrNull(gameObjectName) is not { transform: var targetTransform })
-        {
-            return NotFound($"gameObject '{gameObjectName}' not found");
-        }
+        using var recorderScope = services.StartScope(RecorderScope);
 
-        if (gameObjects.FindOrNull(request.Parent) is not { transform: var parentTransform })
-        {
-            return BadRequest($"gameObject '{request.Parent}' not found");
-        }
+        var handler = recorderScope
+            .ResolveAll<IPostRecorderHandler>()
+            .FirstOrDefault(h => h.Property == property);
 
-        var transformGetter = () =>
+        return handler switch
         {
-            if (targetTransform == null || parentTransform == null)
-            {
-                return null;
-            }
-
-            return new TransformDTO()
-            {
-                Position = parentTransform.InverseTransformPoint(targetTransform.position),
-                Rotation = parentTransform.InverseTransformRotation(targetTransform.rotation),
-                Scale = targetTransform.lossyScale,
-            };
+            IPostRecorderHandler<GameObject> gh => gh.HandleRequest(gameObject),
+            IPostRecorderHandler<Transform> th => th.HandleRequest(gameObject.transform),
+            _ => BadRequest($"property '{property}' is not recordable"),
         };
+    }
 
-        sceneRecorderBuilder.WithRecorder(
-            new JsonRecorder.Builder(targetFile: request.OutputPath, transformGetter)
-                .WithJsonSerializer(jsonSerializer)
-                .WithAdditionalProperty("parent", request.Parent)
+    private static string ReadPropertyFromBody(Request request, JsonSerializer jsonSerializer)
+    {
+        var requestBase = jsonSerializer.IgnoreMissingMembers(
+            () => jsonSerializer.Deserialize<BaseRequestBody>(request.BodyReader)
         );
 
-        return Created();
+        if (requestBase is null)
+        {
+            throw new ResponseException(BadRequest("invalid request body"));
+        }
+
+        request.BodyReader.BaseStream.Position = 0;
+
+        return requestBase.Property;
+    }
+
+    private interface IPostRecorderHandler
+    {
+        public string Property { get; }
+    }
+
+    private interface IPostRecorderHandler<E> : IPostRecorderHandler
+    {
+        public IResponse HandleRequest(E entity);
+    }
+
+    private abstract class PostRecorderHandler<E, B> : IPostRecorderHandler<E>
+        where B : BaseRequestBody
+    {
+        public string Property { get; }
+
+        public required Request Request { protected get; init; }
+
+        public required JsonSerializer JsonSerializer { protected get; init; }
+
+        public required ApiResourceRepository ApiResources { protected get; init; }
+
+        public PostRecorderHandler(string property)
+        {
+            Property = property;
+        }
+
+        public IResponse HandleRequest(E entity)
+        {
+            if (
+                ApiResources.GlobalContainer.GetResource<SceneRecorder.Builder>()
+                is not { } sceneRecorderBuilder
+            )
+            {
+                return ServiceUnavailable();
+            }
+
+            if (JsonSerializer.Deserialize<B>(Request.BodyReader) is not { } requestBody)
+            {
+                return BadRequest("invalid request body");
+            }
+
+            var (response, recorder) = CreateRecorder(entity, requestBody);
+
+            if (recorder is not null)
+            {
+                sceneRecorderBuilder.WithRecorder(recorder);
+            }
+
+            return response;
+        }
+
+        protected abstract (IResponse, IRecorder.IBuilder?) CreateRecorder(E entity, B requestBody);
+    }
+
+    private sealed class CreateTextureRecorderRequest : BaseRequestBody
+    {
+        public required string OutputPath { get; init; }
+
+        public required string Format { get; init; }
+
+        public int ConstantRateFactor { get; init; } = 18;
+    }
+
+    private abstract class PostRenderTextureRecorderHandler(string property)
+        : PostRecorderHandler<ISceneCamera, CreateTextureRecorderRequest>(property)
+    {
+        protected sealed override (IResponse, IRecorder.IBuilder?) CreateRecorder(
+            ISceneCamera camera,
+            CreateTextureRecorderRequest requestBody
+        )
+        {
+            if (FFmpeg.CheckInstallation() is { } exception)
+            {
+                return (
+                    ServiceUnavailable(
+                        new { Error = "ffmpeg is not available", Exception = exception }
+                    ),
+                    null
+                );
+            }
+
+            if (
+                ApiResources.GlobalContainer.GetResource<SceneRecorder.Builder>()
+                is not { } sceneRecorderBuilder
+            )
+            {
+                return (ServiceUnavailable(), null);
+            }
+
+            if (requestBody.Format is not "mp4")
+            {
+                return (BadRequest($"format '{requestBody.Format}' is not supported"), null);
+            }
+
+            if (requestBody.ConstantRateFactor is < 0 or > 63)
+            {
+                return (BadRequest("unsupported constant rate factor value"), null);
+            }
+
+            if (GetRenderTexture(camera) is not { } renderTexture)
+            {
+                return (BadRequest($"camera cannot record {Property}"), null);
+            }
+
+            var recorder = new RenderTextureRecorder.Builder(requestBody.OutputPath, renderTexture)
+                .WithFrameRate(sceneRecorderBuilder.CaptureFrameRate)
+                .WithConstantRateFactor(requestBody.ConstantRateFactor);
+
+            return (Created(), recorder);
+        }
+
+        protected abstract RenderTexture? GetRenderTexture(ISceneCamera camera);
+    }
+
+    private sealed class PostColorTextureRecorderHandler()
+        : PostRenderTextureRecorderHandler("renderTexture.color")
+    {
+        protected override RenderTexture? GetRenderTexture(ISceneCamera camera) =>
+            camera.ColorTexture;
+    }
+
+    private sealed class PostDepthTextureRecorderHandler()
+        : PostRenderTextureRecorderHandler("renderTexture.depth")
+    {
+        protected override RenderTexture? GetRenderTexture(ISceneCamera camera) =>
+            camera.DepthTexture;
+    }
+
+    private sealed class CreateTransformRecorderRequest : BaseRequestBody
+    {
+        public required string OutputPath { get; init; }
+
+        public required string Format { get; init; }
+
+        public required string Parent { get; init; }
+    }
+
+    private sealed class PostTransformRecorderHandler()
+        : PostRecorderHandler<Transform, CreateTransformRecorderRequest>("transform")
+    {
+        public required GameObjectRepository GameObjects { private get; init; }
+
+        protected override (IResponse, IRecorder.IBuilder?) CreateRecorder(
+            Transform transform,
+            CreateTransformRecorderRequest requestBody
+        )
+        {
+            if (requestBody.Format is not "json")
+            {
+                return (BadRequest($"format '{requestBody.Format}' is not supported"), null);
+            }
+
+            if (GameObjects.FindOrNull(requestBody.Parent) is not { transform: var parent })
+            {
+                return (BadRequest($"gameObject '{requestBody.Parent}' not found"), null);
+            }
+
+            var transformGetter = () =>
+            {
+                if (transform == null || parent == null)
+                {
+                    return null;
+                }
+
+                return new TransformDTO()
+                {
+                    Position = parent.InverseTransformPoint(transform.position),
+                    Rotation = parent.InverseTransformRotation(transform.rotation),
+                    Scale = transform.lossyScale,
+                };
+            };
+
+            var recorder = new JsonRecorder.Builder(requestBody.OutputPath, transformGetter)
+                .WithJsonSerializer(JsonSerializer)
+                .WithAdditionalProperty("parent", requestBody.Parent);
+
+            return (Created(), recorder);
+        }
     }
 }
